e3eaa1d40ded00f64cd9346eab217653e5a1fe39
masuda <masuda.fe713@gmail.com>
2025-02-03 23:56:09 +0900

    add masterkey

--- db/schema.rb
+++ db/schema.rb
@@ -10,79 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_02_153118) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_categories_on_name", unique: true
-  end
-
-  create_table "ingredients", force: :cascade do |t|
-    t.uuid "recipe_id", null: false
-    t.string "name", null: false
-    t.string "quantity", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
-  end
-
-  create_table "recipes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "user_id"
-    t.bigint "category_id"
-    t.string "title", null: false
-    t.text "description", null: false
-    t.text "image"
-    t.bigint "views", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["category_id"], name: "index_recipes_on_category_id"
-    t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.uuid "user_id", null: false
-    t.uuid "recipe_id", null: false
-    t.integer "rating", limit: 2, null: false
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["recipe_id"], name: "index_reviews_on_recipe_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
-  create_table "steps", force: :cascade do |t|
-    t.uuid "recipe_id", null: false
-    t.integer "step_number", null: false
-    t.text "description", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipe_id", "step_number"], name: "index_steps_on_recipe_id_and_step_number", unique: true
-  end
-
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
-    t.string "email", limit: 255, null: false
-    t.datetime "email_verified_at", precision: nil
-    t.string "password", limit: 255, null: false
-    t.string "remember_token", limit: 100
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at", precision: nil
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
-  add_foreign_key "ingredients", "recipes", on_delete: :cascade
-  add_foreign_key "recipes", "categories", on_delete: :cascade
-  add_foreign_key "recipes", "users"
-  add_foreign_key "reviews", "recipes", on_delete: :cascade
-  add_foreign_key "reviews", "users", on_delete: :cascade
-  add_foreign_key "steps", "recipes", on_delete: :cascade
 end
